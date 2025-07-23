feeaf9b63adb87d6309292999a4051905358903e
kyin <kiyn2403@gmail.com>
2025-07-23 22:48:35 +0900

    Day 12

--- Day 12/Namespaces and Scope/task.py
+++ Day 12/Namespaces and Scope/task.py
@@ -1,10 +1,11 @@
-enemies = 1
+# Scope = 범위
+enemies = 1 # Global variable
 
-
+# Difference between global and local scope
+# -> Where you define or where you create your variables or your function
 def increase_enemies():
-    enemies = 2
+    enemies = 2 # Local variable
     print(f"enemies inside function: {enemies}")
-
 
 increase_enemies()
 print(f"enemies outside function: {enemies}")
